b92fc0babd90780fa2a38ed38f1335f82adfcdb8
rohitjoshi21 <joshi.rk11166@gmail.com>
2024-10-19 13:19:24 +0545

    room view adding

--- chat/views.py
+++ chat/views.py
@@ -3,14 +3,16 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Message
 from django.views import View
-# Create your views here.
+
+
 class ChatView(LoginRequiredMixin, TemplateView):
     template_name="chat/chat.html"
     login_url="/accounts/login/"
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["messages"] = Message.objects.all().order_by('timestamp')
-        return context
+
+    # def get_context_data(self, **kwargs):
+    #     context =  super().get_context_data(**kwargs)
+    #     context["messages"] = Message.objects.all().order_by('timestamp')
+    #     return context
     
 #to save it to db
 class SendMessageView(LoginRequiredMixin,View):
@@ -19,7 +21,7 @@ class SendMessageView(LoginRequiredMixin,View):
         Message.objects.create(user=request.user,content=content)
         return redirect('chat')
     
-def room(request,room_name):
-    return render(request,"chat/room.html",{
-        "room_name":room_name
-    })
+
+# def room(request,room_name):
+#     print("Entered a room")
+#     return render(request, "chat/room.html")
