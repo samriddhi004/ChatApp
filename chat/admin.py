c3e59d9ba4bcd9e7425c97b038734e483c1aff39
rohitjoshi21 <joshi.rk11166@gmail.com>
2024-10-19 12:33:02 +0545

    models created for message and chatbox

--- chat/admin.py
+++ chat/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Message
 # Register your models here.
 
-class MessageAdmin(admin.ModelAdmin):
-    list_display=("user","content","timestamp")
+# class MessageAdmin(admin.ModelAdmin):
+#     list_display=("user","content","timestamp")
     
-admin.site.register(Message,MessageAdmin)
+# admin.site.register(Message,MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Message(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    sender = models.ForeignKey(User,on_delete=models.CASCADE) #need to use other than CASCADE, as message can be there even if user is deleted
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    chatbox = models.ForeignKey('ChatBox',on_delete=models.CASCADE,null=True)
     
+
     def __str__(self):
-        return f"{self.user} : {self.content}"
+        return f"{self.sender} : {self.content}"
+    
+class ChatBox(models.Model):
+    chatname = models.CharField(max_length=20)
+    members = models.ManyToManyField(User)
+    is_group = models.BooleanField(default=False)
+    is_private = models.BooleanField(default=False)
+    creator = models.ForeignKey(User,null=True,on_delete=models.SET_NULL,related_name="groupscreated") #chat will exist even if admin deleted their account
+    last_update = models.DateTimeField(auto_now=True)
+
